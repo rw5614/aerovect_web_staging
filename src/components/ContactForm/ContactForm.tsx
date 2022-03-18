2f5bdfa137ca74d668e5d4748746f3e7dfdfe6ed
Eric Xie <ericxie1998@gmail.com>
2022-03-18 04:42:46 -0700

    add contact form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -15,16 +15,21 @@ const ContactForm: React.FC<ContactFormProps> = ({
 }) => {
     const t = '';
     return (
-        <div className={`contactFormContainer ${dark ? 'darkMode' : ''}`}>
-            <input placeholder="First Name" />
-            <input placeholder="Last Name" />
-            <input placeholder="Email" />
-            <input placeholder="Subject" />
-            <input placeholder="Message" />
+        <form
+            className={`contactFormContainer ${dark ? 'darkMode' : ''}`}
+            id="contact-form"
+            action="https://formspree.io/xknqngvk"
+            method="POST"
+        >
+            <input id="form_name" type="text" name="name" required placeholder="Name*" />
+            <input id="form_email" type="email" name="email" required placeholder="Email" />
+            <input id="form_subject" type="text" name="subject" required placeholder="Subject" />
+            <input id="form_company" type="text" name="company" required placeholder="Company" />
+            <textarea id="form_message" name="message" required placeholder="Message" />
             <div className="submitWrapper">
-                <button type="button">Send Message</button>
+                <button type="submit" name="submit" id="submit">Send Message</button>
             </div>
-        </div>
+        </form>
     );
 };
 
